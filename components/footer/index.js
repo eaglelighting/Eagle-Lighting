58595602a68de6cb9a084a09c89e678e9c716a9d
Meow Security <meowsecurity@Meows-MacBook-Pro.local>
2022-11-25 13:47:20 -0800

    update fixes and features

--- components/footer/index.js
+++ components/footer/index.js
@@ -14,10 +14,10 @@ const Footer = () => {
             </li>
             <li className="mb-4">
               <a href="#" className="hover:underline">
-                Careers
+                Gallary
               </a>
             </li>
-            <li className="mb-4">
+            {/* <li className="mb-4">
               <a href="#" className="hover:underline">
                 Brand Center
               </a>
@@ -26,7 +26,7 @@ const Footer = () => {
               <a href="#" className="hover:underline">
                 Blog
               </a>
-            </li>
+            </li> */}
           </ul>
         </div>
         <div>
@@ -34,7 +34,7 @@ const Footer = () => {
             Help center
           </h2>
           <ul className="text-gray-500 dark:text-gray-400">
-            <li className="mb-4">
+            {/* <li className="mb-4">
               <a href="#" className="hover:underline">
                 Discord Server
               </a>
@@ -48,7 +48,7 @@ const Footer = () => {
               <a href="#" className="hover:underline">
                 Facebook
               </a>
-            </li>
+            </li> */}
             <li className="mb-4">
               <a href="#" className="hover:underline">
                 Contact Us
@@ -66,7 +66,7 @@ const Footer = () => {
                 Privacy Policy
               </a>
             </li>
-            <li className="mb-4">
+            {/* <li className="mb-4">
               <a href="#" className="hover:underline">
                 Licensing
               </a>
@@ -75,32 +75,32 @@ const Footer = () => {
               <a href="#" className="hover:underline">
                 Terms &amp; Conditions
               </a>
-            </li>
+            </li> */}
           </ul>
         </div>
         <div>
           <h2 className="mb-6 text-sm font-semibold text-gray-500 uppercase dark:text-gray-400">
-            Download
+            Contact
           </h2>
           <ul className="text-gray-500 dark:text-gray-400">
             <li className="mb-4">
               <a href="#" className="hover:underline">
-                iOS
+                Phone
               </a>
             </li>
             <li className="mb-4">
               <a href="#" className="hover:underline">
-                Android
+                Email
               </a>
             </li>
             <li className="mb-4">
               <a href="#" className="hover:underline">
-                Windows
+                Address
               </a>
             </li>
             <li className="mb-4">
               <a href="#" className="hover:underline">
-                MacOS
+                24/7
               </a>
             </li>
           </ul>
@@ -108,7 +108,7 @@ const Footer = () => {
       </div>
       <div className="py-6 px-4 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">
-          © 2022 <a href="https://flowbite.com/">Flowbite™</a>. All Rights
+          © {new Date().getFullYear()} <a href="/">EagleLighting™</a>. All Rights
           Reserved.
         </span>
         <div className="flex mt-4 space-x-6 sm:justify-center md:mt-0">
@@ -163,7 +163,7 @@ const Footer = () => {
             <span className="sr-only">Twitter page</span>
           </a>
           <a
-            href="#"
+            href="https://github.com/EpicDevv"
             className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
           >
             <svg
@@ -179,24 +179,6 @@ const Footer = () => {
               />
             </svg>
             <span className="sr-only">GitHub account</span>
-          </a>
-          <a
-            href="#"
-            className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-          >
-            <svg
-              className="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-                clip-rule="evenodd"
-              />
-            </svg>
-            <span className="sr-only">Dribbble account</span>
           </a>
         </div>
       </div>
