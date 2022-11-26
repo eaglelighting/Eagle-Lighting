815e455247157af733527d26a98563f179b008b7
Meow Security <meowsecurity@Meows-MacBook-Pro.local>
2022-11-25 16:14:30 -0800

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,10 @@ interface Props {
 export default function HomePage({ products }: Props) {
   return (
     <div className='min-h-screen'>
+      <div className='max-h-[5px] overflow-hidden'>
+
+        <img className='w-[100%]' src='/candy.png' />
+      </div>
       <div
         className="relative w-full h-[170px] sm:hidden"
       >
@@ -29,10 +33,16 @@ export default function HomePage({ products }: Props) {
           priority
         />
       </div>
-      <div className='p-[40px]'>
-       <h1 className='text-black text-[30px] leading-[1.1] font-semibold'>Easy, Holiday lighting installation.</h1>
-       <p className='text-black opacity-80 mt-[20px] text-[25px] leading-[1]'>Professional lighting in Idaho. Experience with all types of lighting installation.</p>
-      <button className='h-[30px] mt-[20px] rounded-lg text-black border-[1px] border-black font-semibold px-3'>Get a Quote</button>
+      <div className='flex flex-row-reverse'>
+        <div className='hidden sm:flex'>
+          <img className='cover' src="lights.png" />
+        </div>
+
+        <div className='p-[40px]'>
+          <h1 className='text-black text-[30px] leading-[1.1] font-semibold'>Easy, Holiday lighting installation.</h1>
+          <p className='text-black opacity-80 mt-[20px] text-[25px] leading-[1]'>Professional lighting in <span className='font-bold'>Idaho</span>. Experts in all types of lighting installation.</p>
+          <button className='h-[30px] mt-[30px] rounded-lg text-black border-[1px] border-black font-bold px-3'>Get a Quote</button>
+        </div>
       </div>
     </div>
   );
